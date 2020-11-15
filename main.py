7501463138391768d5b0cefa8332e1c7fcf1cd75
nvrbckdown <visionindark@gmail.com>
2020-11-15 20:26:47 +0500

    Refreshed

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import telebot
 TOKEN = "BotFatherdan olingan bot tokeni!"
 bot = telebot.TeleBot(TOKEN)
 
-
+# Start buyrug`iga javob beruvchi funksiya
 @bot.message_handler(commands=["start"])
 def start_command_handler(message):
     bot.send_message(message.chat.id, "Salom, men ask-sado botiman!")
 
 
+# Ask-sado beruvchi funksiya
 @bot.message_handler(content_types=["text"])
 def echo_message_handler(message):
     chat_id = message.chat.id
@@ -16,5 +17,6 @@ def echo_message_handler(message):
     bot.send_message(chat_id, text)
 
 
+# Botni ishga tushiruvchi funksiyani chaqirilishi
 bot.polling()
 
